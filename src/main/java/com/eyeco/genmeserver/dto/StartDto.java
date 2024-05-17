27afa58f1b130245a1a40d787f9029729d65ebf6
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-18 02:54:28 +0900

    (feat) : 시작 요청을 받기 위한 DTO 구현

--- src/main/java/com/eyeco/genmeserver/dto/StartDto.java
+++ src/main/java/com/eyeco/genmeserver/dto/StartDto.java
@@ -1,4 +1,11 @@
 package com.eyeco.genmeserver.dto;
 
+import lombok.Getter;
+
+@Getter
 public class StartDto {
+    String name;
+    String nickname;
+    String maxCount;
+    String invitationCode;
 }
