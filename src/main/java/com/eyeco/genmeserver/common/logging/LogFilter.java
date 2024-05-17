bd9f1f60fb5b52facff73b16d41aeda786bd4ff6
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-18 00:27:50 +0900

    (feat) : Http에 대한 Log 기능 구현

--- src/main/java/com/eyeco/genmeserver/common/logging/LogFilter.java
+++ src/main/java/com/eyeco/genmeserver/common/logging/LogFilter.java
@@ -4,14 +4,26 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 public class LogFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        CachedHttpRequestServlet cachedReq = new CachedHttpRequestServlet((HttpServletRequest)request);
+        HttpServletResponse res = (HttpServletResponse)response;
 
+        String requestUri = cachedReq.getRequestURI();
+        String requestMethod = cachedReq.getMethod();
+        String requestBody = cachedReq.getCachedRequestData();
+
+        log.info("---Method({}), Request({}) 필터---", requestMethod, requestUri);
+        log.info("requestBodyData : \n{}", requestBody);
+        filterChain.doFilter(cachedReq, res);
+        log.info("---Method({}), Request({}) 필터---", requestMethod, requestUri);
     }
 }
