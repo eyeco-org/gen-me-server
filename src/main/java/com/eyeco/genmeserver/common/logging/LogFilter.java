8b54644c13d3c2617ffdc4d694db3886eb8afa08
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-17 23:53:51 +0900

    (feat) : Log 필터 기능 구현

--- src/main/java/com/eyeco/genmeserver/common/logging/LogFilter.java
+++ src/main/java/com/eyeco/genmeserver/common/logging/LogFilter.java
@@ -1,5 +1,17 @@
 package com.eyeco.genmeserver.common.logging;
 
-public class LogFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
+
+public class LogFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
