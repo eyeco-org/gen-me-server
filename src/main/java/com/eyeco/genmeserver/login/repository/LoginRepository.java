b0568937da35274f7dbf95e8213ca225d7b0a8c6
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-18 06:19:40 +0900

    아이디 중복 검사

--- src/main/java/com/eyeco/genmeserver/login/repository/LoginRepository.java
+++ src/main/java/com/eyeco/genmeserver/login/repository/LoginRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface LoginRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByNickname(User user);
+    boolean existsByNickname(String nickname);
 }
