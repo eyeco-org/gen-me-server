63ac0f897d82672315140f3574e2369e1c78bf48
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-18 02:55:38 +0900

    (feat) : 게임 시작 전 로그인 컨트롤러

--- src/main/java/com/eyeco/genmeserver/login/LoginController.java
+++ src/main/java/com/eyeco/genmeserver/login/LoginController.java
@@ -1,4 +1,28 @@
 package com.eyeco.genmeserver.login;
 
+import com.eyeco.genmeserver.dto.ResponseDto;
+import com.eyeco.genmeserver.dto.StartDto;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/start")
 public class LoginController {
+
+    @PostMapping()
+    public ResponseEntity<ResponseDto<String>> startGame(@RequestBody StartDto startDto){
+        if(startDto.getMaxCount() != null){
+            
+        }
+        else if(startDto.getInvitationCode() != null){
+
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(ResponseDto.success("방을 생성하거나, 입장해야합니다."));
+
+    }
 }
