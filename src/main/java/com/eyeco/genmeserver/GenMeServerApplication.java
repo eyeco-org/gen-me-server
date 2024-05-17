8c9d728ea04e40b84fb045b30f45d1adbc902cb8
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-05-17 22:19:47 +0900

    🎉 start project!

---
+++ src/main/java/com/eyeco/genmeserver/GenMeServerApplication.java
@@ -0,0 +1,13 @@
+package com.eyeco.genmeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenMeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenMeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eyeco/genmeserver/GenMeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyeco.genmeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenMeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
